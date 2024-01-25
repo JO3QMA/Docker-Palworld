1a938b30fe3b7de04bf16668641f304d5a36fbac
JO3QMA <gumi@jo3qma.com>
2024-01-25 22:53:53 +0900

    [Add] Startup options.

--- build/start.sh
+++ build/start.sh
@@ -1,14 +1,50 @@
 #!/bin/bash
 
+# Argument
+SV_Arg=""
+
+if [ "${MULTITHREAD}" = true ]; then
+  SV_Arg+="-useperfthreads -NoAsyncLoadingThread -UseMultithreadForDS"
+fi
+
+if [ -n "${PORT}" ]; then
+  SV_Arg+="-port=${PORT}"
+fi
+
+if [ -n "${PLAYERS}" ]; then
+  SV_Arg+="-players=${PLAYERS}"
+fi
+
+if [ -n "${SERVER_NAME}" ]; then
+  SV_Arg+="-servername=${SERVER_NAME}"
+fi
+
+if [ -n "${SERVER_PASS}" ]; then
+  SV_Arg+="-serverpassword=${SERVER_PASS}"
+fi
+
+if [ -n "${ADMIN_PASS}" ]; then
+  SV_Arg+="-adminpassword=${ADMIN_PASS}"
+fi
+
+if [ "${COMMUNITY_SERVER}" = true ]; then
+  SV_Arg+="EpicApp=PalServer"
+  if [ -n "${PUBLIC_IP}" ]; then
+    SV_Arg+="-publicip=${PUBLIC_IP}"
+  fi
+  if [ -n "${PUBLIC_PORT}" ]; then
+    SV_Arg+="-publicport=${PUBLIC_PORT}"
+  fi
+fi
+
+# Show Server Argument
+echo "Server Arg: ${SV_Arg}"
+
 # Start Pal Server
 echo "[PalServer] Starting..."
 
 /home/steam/palworld/PalServer.sh \
-  EpicApp=PalServer \
-  -publicport=8211 \
-  -useperfthreads \
-  -NoAsyncLoadingThread \
-  -UseMultithreadForDS &
+  "${SV_Arg}" &
 echo $! > /home/steam/palworld.pid
 
 echo "[PalServer] Started!"
